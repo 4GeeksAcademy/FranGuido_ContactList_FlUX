1c06dae27fdbae0848c4c484e956baa369ea764c
Fran Guido <114300538+francinieguido@users.noreply.github.com>
2023-07-31 20:48:57 +0000

    Set add, delete and edit functionalities

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -36,7 +36,50 @@ const getState = ({ getStore, getActions, setStore }) => {
 
 		actions: {
 
+				// Adding new contact to the list
+				addContact: (contact) =>{
+					// Get from store
+					let contactList = getStore().contacts;
+					const newContact = {id: contactList.length +1, ...contact};
+					// Adds one more contacto to the list
+					setStore({ contacts: [...contactList, newContact]});
+				},
 
+				// Editing contact from the list
+				editContact: (id, editedContact) =>{
+					// Get from store
+					let contactList = getStore().contacts;
+					// Find contact index and update
+					const contactIndex = contactList.findIndex(contact => contact.id === id);
+					if (contactIndex !== -1){
+						const editedContacts = [...contactList];
+						editedContacts[contactIndex] = {id, ...editedContact};
+						setStore({ contacts: editedContacts});
+					}
+				},
+
+
+				// Deleting contact
+
+
+
+
+
+
+				// Checking filled form fields when adding or editing
+				// Modal as alert
+				toggleModal: (show) =>{
+					setStore({ showModal: show})
+				},
+
+				checkEmptyFields: (newContact) =>{
+					const {name, email, phone, address} = newContact
+					if (name && email && phone && address) {
+						getActions().addContact(newContact);
+					}
+					else{getActions().toggleModal(true)}
+				},
+				closeModal: () => {setStore({showModal: false})},
 
 
 
